ccbc1858cdc9cc0596d27d6cf51dab9777a97cb1
vilias <vilia_s@hotmail.com>
2019-02-28 09:17:52 +0700

    move constants to common code

---
+++ common/src/commonMain/kotlin/org/jetbrains/kotlinconf/Common.kt
@@ -0,0 +1,5 @@
+package org.jetbrains.kotlinconf
+
+val bcPolicyKey: String = "BCpkADawqM3n0ImwKortQqSZCgJMcyVbb8lJVwt0z16UD0a_h8MpEYcHyKbM8CGOPxBRp0nfSVdfokXBrUu3Sso7Nujv3dnLo0JxC_lNXCl88O7NJ0PR0z2AprnJ_Lwnq7nTcy1GBUrQPr5e"
+val bcAccId: String = "4800266849001"
+val bcVideoId: String = "5255514387001"
--- konfios/konfswift/ui/SessionViewController.swift
+++ konfios/konfswift/ui/SessionViewController.swift
@@ -5,10 +5,6 @@ import PopupDialog
 import main
 import BrightcovePlayerSDK
 
-let kViewControllerPlaybackServicePolicyKey = "BCpkADawqM3n0ImwKortQqSZCgJMcyVbb8lJVwt0z16UD0a_h8MpEYcHyKbM8CGOPxBRp0nfSVdfokXBrUu3Sso7Nujv3dnLo0JxC_lNXCl88O7NJ0PR0z2AprnJ_Lwnq7nTcy1GBUrQPr5e"
-let kViewControllerAccountID = "4800266849001"
-let kViewControllerVideoID = "5255514387001"
-
 class SessionViewController : UIViewController, SessionDetailsView {
 
     private let repository = AppDelegate.me.konfService
@@ -46,7 +42,7 @@ class SessionViewController : UIViewController, SessionDetailsView {
     @IBOutlet private weak var videoContainerView: UIView!
     
     let sharedSDKManager = BCOVPlayerSDKManager.shared()
-    let playbackService = BCOVPlaybackService(accountId: kViewControllerAccountID, policyKey: kViewControllerPlaybackServicePolicyKey)
+    let playbackService = BCOVPlaybackService(accountId: CommonKt.bcAccId, policyKey: CommonKt.bcPolicyKey)
     let playbackController :BCOVPlaybackController
     
     required init?(coder aDecoder: NSCoder) {
@@ -185,7 +181,7 @@ class SessionViewController : UIViewController, SessionDetailsView {
     }
     
     func requestContentFromPlaybackService() {
-        playbackService?.findVideo(withVideoID: kViewControllerVideoID, parameters: nil) { (video: BCOVVideo?, jsonResponse: [AnyHashable: Any]?, error: Error?) -> Void in
+        playbackService?.findVideo(withVideoID: CommonKt.bcVideoId, parameters: nil) { (video: BCOVVideo?, jsonResponse: [AnyHashable: Any]?, error: Error?) -> Void in
             
             if let v = video {
                 self.playbackController.setVideos([v] as NSArray)
